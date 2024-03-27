0e2d9d209a295e6db10e353e367d8792d4535408
dlekdus4902 <rosemaridy@naver.com>
2024-03-27 19:36:14 +0900

    3주차

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const button1 = document.querySelector("#추가버튼");
 const input1 = document.querySelector("#입력값");
+const button1 = document.querySelector("#추가버튼");
 const todos = document.querySelector("#할일들");
 
 button1.addEventListener("click", () => {
@@ -8,4 +8,15 @@ button1.addEventListener("click", () => {
   p1.innerText = value1;
   todos.appendChild(p1);
   input1.value = "";
+
+  p1.addEventListener("click", () => {
+    //조건문
+    //1. done이 없으면 done을 추가
+    //2. 그게 아니면 done을 없앰
+    if (p1.className !== "done") {
+      p1.className = "done";
+    } else {
+      p1.className = "";
+    }
+  });
 });
